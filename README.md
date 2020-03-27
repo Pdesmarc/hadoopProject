57d1e9f3a0f2363a981e63a3d674aab2ee22d502
Pierre Desmarchelier <desmarchelier.p21@gmail.com>
2020-03-27 21:57:14 +0000

    cleaning the repo and update of the README

--- README.md
+++ README.md
@@ -1,23 +1,88 @@
-Bienvenue sur notre projet Hadoop : ligne de transilien N
+# Projet Hadoop
+
+The goal of Hadoop project is to get in real time the average waiting time for each stations of the N line using: 
+- RATP API
+- Kafka
+- SparkStreaming
+- Tableau software
 
 MS SIO 2019-2020 - CentraleSupélec
 
-Membres de l'équipe : 
+Team Members : 
 DESMARCHELIER Pierre,
 LY Stéphane,
 NGBANGO Soulémanou,
 RHERMINI Acheq
 
-# Installation : 
+## Installation
 
-1) Installer l'outil Virtualbox : https://www.virtualbox.org/wiki/Downloads (alternativement, on peut également passer par une instance EC2)
-2) Télécharger la version 2.6.5 de la sandbox HDP : https://www.cloudera.com/downloads/hortonworks-sandbox.html
-3) Importer dans la machine virtuelle et installer HDP
-4) Se connecter à sa machine virtuelle en SSH
-5) Lancer le docker engine avec la commande sudo dockerd
-6) Lancer le docker hdp puis le docker proxy avec la commande docker start
-7) Entrer dans le docker hdp avec la commande docker exec
-8) Cloner ce repository dans /usr/hdp/current/KafkaBroker
-9) ...
+### Hadoop stack 
 
-Note : afin de pouvoir se connecter à HDFS et faire les requêtes avec Tableau, un plugin spécifique peut être nécessaire : https://www.cloudera.com/downloads/hdp.html
+Please follow this procedure to install HDP 3.0.1 Sandbox Through Docker on AWS : https://github.com/qge/hdp 
+
+### Reproduce our environnement
+
+You need to first launch your AWS EC2 instance. Once your instance is launched : 
+```sh
+$ ssh -i "EPATH/TO/YOUR/KEY.pem" ec2-user@YOUR-PUBLIC-DNS-ADRESS
+```
+
+Then you need to start the dockers:
+```sh
+# Start the docker daemons engine
+$ sudo systemctl start docker
+
+#Start the docker containing hadoop
+$ sudo docker start <HDP-DOCKER-ID>
+
+#Start the docker containing the proxy
+$ sudo docker start <PROXY-DOCKER-ID>
+```
+
+Now you should be able to access into the Hadoop docker 
+```sh
+# Access the hadoop docker
+$ docker exec -it <HDP-DOCKER-ID> /bin/bash
+```
+
+Once you are in the docker, please go inside the kafka_broker repository :
+```sh
+# Access the kafka_broker repository 
+$ cd /usr/hdp/current/kafka-broker/
+```
+Then clone our repo : 
+```sh
+# Clone our git repo 
+$ git clone https://github.com/Pdesmarc/hadoopProject.git
+$ cd hadoopProjet/
+```
+###  Save to hdfs
+The spark streaming script need to have an example file with the response of the RATP API in hdfs (user/root/). We provide in this repo the example file : test.json.
+```sh
+# Save to hdfs (/user/root/)
+$ hdfs dfs -put test2.json /user/root
+# To check if the file is in hdfs
+$ hdfs dfs -ls /user/root/
+```
+
+ ## Kafka
+ 
+ ### Run the producer python script
+  You have nothing to do, the script will retrieve all the authorizations needed to do a request.
+```sh
+# Producer script
+$ python app1-ratpProducer.py
+```
+### Launch the spark submit 
+To launch the spark submit, you must to write this command : 
+```sh
+# Launch the spark submit 
+$ spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 sparkstream_script.py
+```
+Once the producer and the spark submit are launched, spark streaming will automatically save the results in hdfs : /user/root/save_stream/ . The results are saved in parquet. 
+
+
+## Tableau
+
+To allow Tableau software  to connect HDFS, please install this plugins : https://www.cloudera.com/downloads/hdp.html
+
--- sparkstream_script.py
+++ sparkstream_script.py
@@ -9,28 +9,25 @@ if __name__ == '__main__':
 	spark = SparkSession.builder.appName("HadoopProject").enableHiveSupport().getOrCreate() 
 
 	#On instancie notre stream en souscrivant au topic ratp-api
-	kafkaStream = spark.readStream.format("kafka").option("kafka.bootstrap.servers","sandbox-hdp.hortonworks.com:9092").option("subscribe","ratp-api").load() #.option("startingOffsets", "earliest")
+	kafkaStream = spark.readStream.format("kafka").option("kafka.bootstrap.servers","sandbox-hdp.hortonworks.com:9092").option("subscribe","ratp-api").load()
 
-
-	#On vient recuperer la requete example pour recuperer le schema global
+	#On vient recuperer la requete type pour recuperer le schema global
 	json_example = spark.read.json("hdfs:///user/root/test.json",multiLine=True)
 	schema_json=json_example.schema
-	#json_example.printSchema()
 
-	#On recupere les colonnes voulues des messages du producer : value corresponds a la reponse, et timestamp a l'heure ou a ete effectue la requete
+	#On recupere les colonnes voulues des messages du producer : value correspond a la reponse, et timestamp a l'heure ou a ete effectue la requete
 	df = kafkaStream.selectExpr("CAST(value as STRING)", "CAST(timestamp as TIMESTAMP)")
 
 	#Selection de la partie du json qui nous interesse
 	df = df.select(F.from_json(F.col("value"), schema_json).alias("test"), "timestamp").select("test.Siri.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit","timestamp").select(F.explode("MonitoredStopVisit"), "timestamp").select(F.explode("col"),"timestamp").select("col.MonitoredVehicleJourney","timestamp", "col.MonitoringRef")
-	#df.printSchema()
 
-	#Selectionne des colonnes qui nous interesse
+	#On selectionne les colonnes qui nous interessent
 	df = df.select("MonitoredVehicleJourney.DestinationName","MonitoredVehicleJourney.LineRef","MonitoredVehicleJourney.TrainNumbers.TrainNumberRef", "MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime", "MonitoredVehicleJourney.MonitoredCall.StopPointName", "timestamp", "MonitoringRef")
 
 	#On renomme et on met toutes les donnees au meme niveau
 	df= df.withColumn("Destination", F.explode("DestinationName")).withColumn("TrainNumber", F.explode("TrainNumberRef")).withColumn("StopName", F.explode("StopPointName")).drop("DestinationName","TrainNumberRef", "StopPointName")
 
-	#Pour une meilleure visualisation, on met les dans dans un ordre precis
+	#Pour une meilleure visualisation, on met les valeurs dans dans un ordre precis
 	df = df.select("timestamp",F.col("TrainNumber.value").alias("TrainNumber"),F.col("StopName.value").alias("StopName"),F.col("MonitoringRef.value").alias("MonitoringRef"), "ExpectedArrivalTime", F.col("Destination.value").alias("Destination"),F.col("LineRef.value").alias("LineRef"))
 
 	#Nettoyage et filtrage des donnees sur la ligne N, sur les heures erronees et sur les possibilites de duplicat
@@ -41,12 +38,12 @@ if __name__ == '__main__':
 
 
 
-	#On definit arbitrairement un watermark de 5 minutes, et on regroupe par station avec une fenetre d'1h comme demande avec un intervalle glissant de 5 minutes pour avoir le resultat heure par heure
+	#On definit arbitrairement un watermark de 5 minutes, et on regroupe par station avec une fenetre d'1h comme demande avec un intervalle glissant de 2 minutes
 	df = df.withWatermark("timestamp", "5 minutes").groupBy(F.window("timestamp", "60 minutes", "2 minutes"), "StopName", "MonitoringRef")
 
-	#On definit le temps d'attente moyen par : 1h divise par le nombre de train passe a la station l'heure precedent
+	#On definit le temps d'attente moyen par : 1h divise par le nombre de train qui passe a la station durant l'heure en question
 	df = df.agg(F.count("TrainNumber").alias("NumberOfTrain"), F.format_number(60./ F.count("TrainNumber"), 2).cast("long").alias("AverageWaitingTime"))
 
-
+	# On vient sauver les resultat dans hdfs en parquet 
 	query = df.select("StopName", "MonitoringRef", "window", "NumberOfTrain", "AverageWaitingTime").writeStream.format("console").option("truncate", False).outputMode("append").format("parquet").option("checkpointLocation", "stream_checkpoint").option("path", "stream_save").start()
 	query.awaitTermination()
